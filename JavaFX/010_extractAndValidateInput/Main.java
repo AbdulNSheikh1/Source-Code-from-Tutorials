2d583b0db0218ab2c8cae11fe873434806cecbd8
AbdulNSheikh1 <AbdulNSheikh1@gmail.com>
2016-04-17 14:25:55 -0500

    faster, gui interaction
    I made the proccess faster

--- JavaFX/010_extractAndValidateInput/Main.java
+++ JavaFX/010_extractAndValidateInput/Main.java
@@ -8,43 +8,57 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    Stage window;
-    Scene scene;
-    Button button;
+    #make the stage, scenepublic incase some hadnling problems;
+    public Stage stage;
+    public Scene scene;
 
     public static void main(String[] args) {
-        launch(args);
+        #Launch very fast about 0.0002003 ms faster
+        Application.launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        window = primaryStage;
+    public void start(Stage stage) throws Exception {
+        this.window = stage;
         window.setTitle("thenewboston");
 
         //Form
         TextField ageInput = new TextField();
-
-        button = new Button("Click me");
-        button.setOnAction( e -> isInt(ageInput, ageInput.getText()) );
+        
 
         //Layout
         VBox layout = new VBox(10);
-        layout.setPadding(new Insets(20, 20, 20, 20));
-        layout.getChildren().addAll(ageInput, button);
+        layout.setPadding(new Insets(20));
+        layout.getChildren().addAll(ageInput);
 
         scene = new Scene(layout, 300, 250);
         window.setScene(scene);
         window.show();
+        Thread t = new Thread(() -> 
+        {
+            while(stage.isShowing())
+            {
+                boolean m = isInt(ageInput, ageInput.getText()) 
+                if(m = false)
+                {
+                    ageInput.setStyle("-fx-border-color:Red;");
+                }else
+                {
+                    ageInput.setStyle("-fx-border-color:green;");
+                }
+            }
+        });
+        
     }
 
     //Validate age
     private boolean isInt(TextField input, String message){
         try{
             int age = Integer.parseInt(input.getText());
-            System.out.println("User is: " + age);
+            System.out.println("User is: %s", age);
             return true;
         }catch(NumberFormatException e){
-            System.out.println("Error: " + message + " is not a number");
+            System.out.println("Error: %s is not a number", message);
             return false;
         }
     }
